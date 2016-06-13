f67429068d3ef321c15ad0765c644cd4e578500e
Greg Laurette <greglaurette@gmail.com>
2016-06-13 14:46:22 -0300

    add menu

---
+++ AmherstGolfClub/Models/MenuCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmherstGolfClub.Models
+{
+    public class MenuCategory
+    {
+        public int ID { get; set; }
+
+        public string Category { get; set; }
+    }
+}
---
+++ AmherstGolfClub/Models/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AmherstGolfClub.Models
+{
+    public class MenuItem
+    {
+        [Display(Name = "Item ID")]
+        public int MenuItemID { get; set; }
+
+        [Display(Name = "Item Name")]
+        public string ItemName { get; set; }
+
+        [Display(Name = "Price")]
+        public decimal ItemPrice { get; set; }
+
+        [Display(Name = "Category")]
+        [ForeignKey("MenuCategory")]
+        public int Category { get; set; }
+
+        public IEnumerable<MenuItem> MenuItems { get; set; }
+    }
+}
