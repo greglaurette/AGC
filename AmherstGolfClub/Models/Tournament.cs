d81eebab66cf1ab5308ee7ca47410d553b8f7619
Greg Laurette <greglaurette@gmail.com>
2016-06-20 12:10:35 -0300

    Revert "add tourny results and manager"
    This reverts commit 077563d723ea2fee7e64e541a02215a0be01bebd.

--- AmherstGolfClub/Models/Tournament.cs
+++ AmherstGolfClub/Models/Tournament.cs
@@ -9,9 +9,9 @@ namespace AmherstGolfClub.Models
 {
     public class Tournament
     {
-        public int TournamentID { get; set; }    
+        public virtual ICollection<TournamentPlayers> TournamentPlayers { get; set; }
 
-        public string Year { get; set; }
+        public int TournamentID { get; set; }
 
         [Required]
         [Display(Name = "Tournament")]
@@ -22,6 +22,9 @@ namespace AmherstGolfClub.Models
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime TournamentDate { get; set; }
+
+        //[Display(Name = "File Name")]
+        //public File FileName { }
 
     }
 }
