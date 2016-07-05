7e61f0f6c770df5970781bf8f22679f06942b195
Greg <greglaurette@gmail.com>
2016-07-05 13:03:09 -0300

    finished images

--- AmherstGolfClub/Controllers/RatesController.cs
+++ AmherstGolfClub/Controllers/RatesController.cs
@@ -1,8 +1,12 @@
 ﻿using AmherstGolfClub.DAL;
+using AmherstGolfClub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,6 +31,49 @@ namespace AmherstGolfClub.Controllers
         public ActionResult Corp()
         {
             return View(db.Rate.ToList());
+        }
+
+        public ActionResult Events()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Events(ContactForm c)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    MailAddress from = new MailAddress(c.emailAddress.ToString());
+                    StringBuilder sb = new StringBuilder();
+                    msg.IsBodyHtml = false;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Credentials = new NetworkCredential("agcc1909@gmail.com", "#foxRanch3");
+                    msg.To.Add("agcc@eastlink.ca");
+                    msg.From = from;
+                    msg.Subject = c.emailSubject;
+                    sb.Append("Name: " + c.emailName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Message: " + c.emailMessage);
+                    msg.Body = sb.ToString();
+                    smtp.Send(msg);
+                    msg.Dispose();
+                    return View("Success");
+
+                }
+                catch (Exception)
+                {
+                    return View("Error");
+                }
+
+            }
+            return View();
         }
     }
 }
